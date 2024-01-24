08bbb203bc789b2830c207376fc552df0457c6c2
Arsenij Nikulin <arsenij.nikulin@mif.stud.vu.lt>
2024-01-24 22:20:38 +0200

    Discard Django.

--- BonitaVita/main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def about(request):
-    return render(request, 'main/about/about.html')
-
-def contacts(request):
-    return render(request, 'main/contacts/contacts.html')
